9f55bd22afa35c06e5f17d739756f133a464eaeb
Dmitry <dima2323fr3@gmail.com>
2023-12-11 08:41:27 +0700

    Создал интерфейс для работы с Json

---
+++ AutoSystem(CourseWork)/Data/ISerializeBase.cs
@@ -0,0 +1,20 @@
+﻿using AutoSystem_CourseWork_.Model.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSystem_CourseWork_.Data
+{
+    public interface ISerializeBase<T> where T : IEntity
+    {
+        bool Add(T entity);
+        bool Delete(T entity);
+        bool Update(T entity);
+        bool Save();
+        bool Load();
+        Task<bool> SaveAsync();
+        Task<bool> LoadAsync();
+    }
+}
