2536374bc8c269476a63b281cd02653639fb7664
Dmitry <dima2323fr3@gmail.com>
2023-12-11 08:09:46 +0700

    Добавил пару классов и интерфейс в возможности

---
+++ AutoSystem(CourseWork)/Model/Possibilities/CanChangeUserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSystem_CourseWork_.Model.Possibilities
+{
+    public class CanChangeUserRole : IChangeUserRole
+    {
+        public void ChangeUserRole(int Role_Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AutoSystem(CourseWork)/Model/Possibilities/IChangeUserRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSystem_CourseWork_.Model.Possibilities
+{
+    public interface IChangeUserRole
+    {
+        public void ChangeUserRole(int Role_Id);
+    }
+}
