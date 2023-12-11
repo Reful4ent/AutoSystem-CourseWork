7f4ec7d2488401137da38175e43efb90447b41bc
Dmitry <dima2323fr3@gmail.com>
2023-12-11 07:30:59 +0700

    первая фиксация

---
+++ AutoSystem(CourseWork)/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AutoSystem_CourseWork_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
